425139906598a696d5e09de4f7aa6d6b909f93f9
Mutala Mutala <jonamut12.live@gmail.com>
2025-01-21 23:29:34 -0500

    Add User repository

---
+++ Ewenze.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ewenze.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ewenze.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetUsersAsync(); 
+        Task<User> GetUserByEmailAsync(string email);
+        Task<User> GetUserById(int id);
+    }
+}
---
+++ Ewenze.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using Ewenze.Domain.Entities;
+using Ewenze.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ewenze.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<User> GetUserByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
