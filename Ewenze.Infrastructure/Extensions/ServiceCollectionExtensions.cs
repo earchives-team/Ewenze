f9347330788a28b7f56cdc8fbb613669ee807e88
Mutala Mutala <jonamut12.live@gmail.com>
2025-01-21 23:14:10 -0500

    Add services Collection

--- Ewenze.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Ewenze.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,8 +1,21 @@
 ﻿
+using Ewenze.Infrastructure.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Ewenze.Infrastructure.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public 
+        public static IServiceCollection AddEwenzeInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<EWenzeDbContext>(options =>
+            {
+                options.UseMySQL(configuration.GetConnectionString("EWenze")!);
+            });
+
+            return services;
+        }
     }
 }
