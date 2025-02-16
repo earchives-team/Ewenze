18fd835c4cb5e3630f32590ded488ba87a82d4a7
Mutala Mutala <jonamut12.live@gmail.com>
2025-02-16 18:14:19 -0500

    Add Auth controller

---
+++ Ewenze.API/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Ewenze.Application.Authentication;
+using Ewenze.Application.Models.AuthModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ewenze.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public readonly IAuthService _authentificationService;
+
+        public AuthController(IAuthService authentificationService)
+        {
+            _authentificationService = authentificationService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponse>> Login(AuthRequest request)
+        {
+            return Ok(await _authentificationService.Login(request));
+        }
+    }
+}
