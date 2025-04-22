34ec48ee16cfa8a7a99295206804f6ed0b410bf0
Mutala Mutala <jonamut12.live@gmail.com>
2025-04-22 18:26:24 -0400

    Fix endpoints

--- Ewenze.API/Controllers/UsersController.cs
+++ Ewenze.API/Controllers/UsersController.cs
@@ -12,39 +12,42 @@ namespace Ewenze.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-   // [Authorize] // Seuls les utilisateurs authentifiés peuvent accéder
-    public class UserController : ControllerBase
+    // [Authorize] // Seuls les utilisateurs authentifiés peuvent accéder
+    public class UsersController : ControllerBase
     {
         private readonly IMediator _mediator;
 
-        public UserController(IMediator mediator)
+        public UsersController(IMediator mediator)
         {
             this._mediator = mediator;
         }
 
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<UserDto>), 200)]
-        [Route("GetAllUsers")]
-        public async Task<IActionResult> GetUsers()
+        public async Task<IActionResult> Get()
         {
             return Ok( await _mediator.Send(new GetUsersQuery()));
         }
 
+        // GET: api/users/{id}
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            return Ok(await _mediator.Send(new GetUserByIdQuery(id)));
+        }
+
+        // GET: api/users/by-email?email=value
         [HttpGet]
-        [ProducesResponseType(typeof(UserDto),200)]
-        [Route("GetByEmail")]
-        public async Task<IActionResult> GetUserByEmail(string email)
+        [ProducesResponseType(typeof(UserDto), 200)]
+        [Route("by-email")]
+        public async Task<IActionResult> GetUserByEmail([FromQuery]string email)
         {
             return Ok(await _mediator.Send(new GetUserByEmailQuery(email)));
         }
 
-        [HttpGet]
-        [Route("GetById/{id}")]
-        public async Task<IActionResult> GetUserById(int id)
-        {
-            return Ok(await _mediator.Send(new GetUserByIdQuery(id))); 
-        }
-
+        // POST: api/users
         [HttpPost]
         [AllowAnonymous] // Cette action est accessible sans authentification
         [ProducesResponseType(StatusCodes.Status201Created)]
