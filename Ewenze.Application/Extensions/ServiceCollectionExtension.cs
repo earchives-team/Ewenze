8fee6c23a64dc96bb0192b882c65459b23dda267
Mutala Mutala <jonamut12.live@gmail.com>
2025-05-25 03:11:25 -0400

    Registrate service

--- Ewenze.Application/Extensions/ServiceCollectionExtension.cs
+++ Ewenze.Application/Extensions/ServiceCollectionExtension.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using Ewenze.Application.Services.Users;
+using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -18,6 +19,8 @@ namespace Ewenze.Application.Extensions
             services.AddValidatorsFromAssembly(applicationAssembly)
                 .AddFluentValidationAutoValidation();
 
+            services.AddTransient<IUsersService, UsersService>();
+            services.AddSingleton<IUserConverter, UserConverter>();
 
             return services;
         }
